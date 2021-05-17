78b5b4cfee6e8c5bb83c01f4df391d5d7b1d8524
powellmad <madsnpowell@gmail.com>
2021-05-17 16:03:27 -0500

    created json files and began fixtures

---
+++ rareapi/models/post.py
@@ -0,0 +1,14 @@
+from rareapi.models.posttag import PostTag
+from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from django.utils.timezone import now
+class Post(models.Model):
+    user=models.ForeignKey("RareUser", on_delete=CASCADE)
+    category=models.ForeignKey("Category", on_delete=DO_NOTHING)
+    title=models.CharField(max_length=50)
+    publication_date=models.DateTimeField(default=now)
+    image_url=models.ImageField()
+    content=models.CharField(max_length=50)
+    approved=models.BooleanField()
+    tags=models.ManyToManyField("Tag", through="PostTag", related_name="posts")
+    reactions=models.ManyToManyField("Reaction", through="PostReaction", related_name="posts")
