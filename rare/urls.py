55a888fccb1e16e5111a00b9220ad8cf0f08545f
bpotts11 <brittneympotts@gmail.com>
2021-05-19 13:53:05 -0500

    got all posts to render to the DOM

--- rare/urls.py
+++ rare/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rareapi.views.post import PostView
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
@@ -20,6 +21,7 @@ from rest_framework import routers
 from rareapi.views import login_user, register_user
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'', PostView, 'post')
 
 urlpatterns = [
     path('', include(router.urls)),
---
+++ rareapi/views/post.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rareapi.models import Post
+
+
+class PostView(ViewSet):
+
+    def list(self, request):
+        """Handle GET requests to events resource
+        Returns:
+            Response -- JSON serialized list of posts
+        """
+        posts = Post.objects.all()
+        # Note the additional `many=True` argument to the
+        # serializer. It's needed when you are serializing
+        # a list of objects instead of a single object.
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """JSON serializer for posts
+    Arguments:
+        serializer type
+    """
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'category', 'title', 'publication_date',
+                  'image_url', 'content', 'approved', 'tags', 'reactions')
+        depth = 1
